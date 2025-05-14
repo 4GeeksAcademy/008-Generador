44fe331221210f7e5a55b963005c8944e6ce23b8
Yonl21 <yoneikerluna@gmail.com>
2025-05-14 09:40:49 +0000

    generador problema final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  const sujeto = ["Mi tarea", "Spiderman", "Yugoslavia", "El Socialismo del siglo XXI"];
+  const verbo = ["me impidio", "dejo de", "se comio", "se encargo"];
+  const predicado = ["tener alguna utilidad", "continuar con mis objetivos", "se comio a mi perro", "se desintegro"];
+
+  function generator() {
+    const exSujeto = sujeto[Math.floor(Math.random() * sujeto.length)];
+    const exVerbo = verbo[Math.floor(Math.random() * verbo.length)];
+    const exPredicado = predicado[Math.floor(Math.random() * predicado.length)];
+
+    const excuse = `${exSujeto} ${exVerbo} ${exPredicado}`;
+    document.getElementById("excuse").textContent = excuse
+  }
+
+  window.onload = generator
